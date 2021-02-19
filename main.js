9d560de211b6619dde8e50b0a584175b8cbd598b
JDx <abirkoby@gmail.com>
2021-02-19 04:47:59 +0200

    finished controller

--- main.js
+++ main.js
@@ -2,3 +2,23 @@ const tweeter = Tweeter()
 const renderer = Renderer()
 
 renderer.renderPosts(tweeter.getPosts())
+
+let post = function(e) {
+    tweeter.addPost($('#input').val());
+    renderer.renderPosts(tweeter.getPosts())
+}
+
+$('#container').on('click','.delete' ,function(e) {
+    tweeter.removePost($(this).closest('.post').data().id);  
+    renderer.renderPosts(tweeter.getPosts())
+  
+})
+$('#container').on('click','.post-comment',function(e){ 
+    tweeter.addComment($(this).closest('.post').find('.comment-input').val(),$(this).closest('.post').data().id)
+    renderer.renderPosts(tweeter.getPosts())
+})
+$('#container').on('click','.delete-comment',function(e){ 
+    tweeter.removeComment( $(this).closest('.post').data().id,   $(this).closest('.comments').data().id);
+    renderer.renderPosts(tweeter.getPosts())
+
+})
--- render.js
+++ render.js
@@ -4,15 +4,18 @@ let Renderer = function () {
          $('#posts').empty();   
             for (let post of posts) 
             {
-                let $postHtml = $(`<div class="post" data-id="${post.id}" >${post.text}</div>`)
+                let $postHtml = $(`<div class="post" data-id="${post.id}" >${post.text} <i class="delete fas fa-trash"></i></div>`)
                 $('#posts').append(($postHtml));
                 for (let comment of post.comments)
                 {
-                    let $commentHtml = $(`<div class="comments" data-id="${comment.id}">${comment.text}</div>`);
+                    let $commentHtml = $(`<div class="comments" data-id="${comment.id}">${comment.text} <i class="delete-comment fas fa-trash"></i></div>`);
                     $postHtml.append($commentHtml);
                 }
+                let $addCommentHtml= $(`<input type="text" class="comment-input" placeholder="Enter Comment" value=""></input><button class="post-comment">Comment</button>`)
+                $postHtml.append($addCommentHtml);
+
             }
-    
+
     
         }
 
